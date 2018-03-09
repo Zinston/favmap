b78cdd6a8beac7d41d55d2dad1fea6acd8ad88bc
Antoine Guenet <aguenet@gmail.com>
2018-03-09 02:43:49 +0100

    Make the input a Google Places API SearchBox.

--- js/map.js
+++ js/map.js
@@ -6,4 +6,12 @@ function initMap() {
 		center: {lat: 40.7413549, lng: -73.9980244},
 		zoom: 13
 	});
+
+	// Create a searchbox in order to execute a places search
+    var searchBox = new google.maps.places.SearchBox(
+        document.getElementById('places-search'));
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
 };

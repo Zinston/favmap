e6e37a3b51d88b9ec1c01193343967a55e6d3337
Antoine Guenet <aguenet@gmail.com>
2018-03-09 04:58:51 +0100

    Add search and center on place on input.

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,8 +1,28 @@
-var ViewModel = function () {
-	
+function ViewModel() {
+	this.searchInput = ko.observable();
+
+	this.searchPlace = function(place) {
+		var bounds = map.getBounds();
+        var placesService = new google.maps.places.PlacesService(map);
+        placesService.textSearch({
+            query: document.getElementById('places-search').value,
+            bounds: bounds
+        }, function(places, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                var location = places[0].geometry.location;
+			    // Center the map on it and zoom
+			    map.setCenter(location);
+			    map.setZoom(17);
+            }
+        });
+	};
 };
 
 function init() {
 	var vm = new ViewModel();
 	ko.applyBindings(vm);
 };
+
+$(function() {
+	init();
+});
